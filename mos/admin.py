24855e428286e7c76ca166f485c4ab48d747d6c4
Zulfugar028 <gfotograf77@gmail.com>
2026-03-20 12:52:56 +0400

    Changes

--- mos/admin.py
+++ mos/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import ContactRequest
+from .models import Visitor
 
 @admin.register(ContactRequest)
 class ContactRequestAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'email', 'service', 'created_at')
     list_filter = ('service', 'created_at')
     search_fields = ('full_name', 'email', 'message')
+
+@admin.register(Visitor)
+class VisitorAdmin(admin.ModelAdmin):
+    list_display = ('ip_address', 'date')
+    list_filter = ('date',)
+    search_fields = ('ip_address',)
